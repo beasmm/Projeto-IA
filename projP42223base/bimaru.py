c76d07c1ada27ea7330595c82374a1a0fd36cf4e
Beatriz Mira Mendes.git <beatriz.mira.mendes@tecnico.ulisboa.pt>
2023-05-30 12:45:39 +0100

    class board terminado

--- projP42223base/bimaru.py
+++ projP42223base/bimaru.py
@@ -37,13 +37,21 @@ class Board:
 
     def get_value(self, row: int, col: int) -> str:
         """Devolve o valor na respetiva posição do tabuleiro."""
-        # TODO
-        pass
+        if self.board[row][col] == ".":
+            return None
+        else:
+            return self.board[row][col]
 
     def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
         """Devolve os valores imediatamente acima e abaixo,
         respectivamente."""
-        # TODO
+        if self.row == 0 or self.[row - 1][col] == "":
+            return None, self.board[row + 1][col]
+        elif self.row == 9:
+            return self.board[row - 1][col], None
+        
+        elif self.[row + 1][col] == "" and self.[row - 1][col] == "":
+            return None, None
         pass
 
     def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
